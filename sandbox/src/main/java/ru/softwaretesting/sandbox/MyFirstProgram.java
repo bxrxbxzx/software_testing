bf1f71b12d66b712ab911f824a922d0fc2501054
bxrxbxzx <ast_16@mail.ru>
2019-06-05 17:51:33 +0300

    Создал функцию, сделал запускаемый класс.

--- sandbox/src/main/java/ru/softwaretesting/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/softwaretesting/sandbox/MyFirstProgram.java
@@ -7,12 +7,17 @@ public class MyFirstProgram {
         hello("user");
         hello("Alexey");
 
+        Point p1 = new Point(5, 15);
+        Point p2 = new Point(7, 22);
+        System.out.println( "Расстояние между двумя точками равно: " + distance(p1, p2));
+        System.out.println("Расстояние между двумя точками равно: " +  p1.distance(p2));
+
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s ) );
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4,6);
 
-        System.out.println("Площадь прямоугольника со стороноами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадь прямоугольника со стороноами " + r.a + " и " + r.b + " = " + s.area());
 
     }
     public static void hello(String somebody){
@@ -21,14 +26,17 @@ public class MyFirstProgram {
 
     }
 
-    public static double area(Square s){
-        return s.l * s.l;
+    public static double distance(Point p1, Point p2){
+
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+
+
 
     }
-    public static double area(Rectangle r){
-        return r.a * r.b;
 
-    }
+
+
+
 
 
 }
--- sandbox/src/main/java/ru/softwaretesting/sandbox/Point.java
+++ sandbox/src/main/java/ru/softwaretesting/sandbox/Point.java
@@ -1,8 +1,20 @@
 package ru.softwaretesting.sandbox;
 
 public class Point {
-    public static void main(String[] args) {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+
+    }
+    public double distance(Point point){
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
 
 
     }
+
+
 }
