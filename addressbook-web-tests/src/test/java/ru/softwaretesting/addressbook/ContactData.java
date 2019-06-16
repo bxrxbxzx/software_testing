e27a923d4e6551013ddc838e14e1fa2299045ffa
bxrxbxzx <ast_16@mail.ru>
2019-06-16 19:39:39 +0300

    Выделены и параметризованы вспомогательные методы.

---
+++ addressbook-web-tests/src/test/java/ru/softwaretesting/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package ru.softwaretesting.addressbook;
+
+public class ContactData {
+    private final String name;
+    private final String patronimic;
+    private final String surname;
+    private final String AKA;
+    private final String address;
+    private final String mobile;
+    private final String mail;
+    private final String day;
+    private final String month;
+    private final String year;
+
+    public ContactData(String name, String patronimic, String surname, String AKA, String address, String mobile, String mail, String day, String month, String year) {
+        this.name = name;
+        this.patronimic = patronimic;
+        this.surname = surname;
+        this.AKA = AKA;
+        this.address = address;
+        this.mobile = mobile;
+        this.mail = mail;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronimic() {
+        return patronimic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAKA() {
+        return AKA;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
--- addressbook-web-tests/src/test/java/ru/softwaretesting/addressbook/NewContactTests.java
+++ addressbook-web-tests/src/test/java/ru/softwaretesting/addressbook/NewContactTests.java
@@ -4,9 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class NewContactTests {
@@ -18,57 +16,69 @@ public class NewContactTests {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
-        addNew();
+        login("admin", "secret");
+
     }
 
     @Test
     public void testNewContact() {
+        gotoAddNew();
+        fillContactForm(new ContactData("Babushka", "Petrovna", "Shtolz", "BP", "Petrozavodsk, Lenina str. 15, 454", "(888)9991524", "asasas@trtr.net", "15", "April", "1966"));
+        submitContactForm();
+        logOut();
 
+    }
+
+    private void logOut() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    private void submitContactForm() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Babushka");
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
         wd.findElement(By.name("middlename")).click();
         wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys("Petrovna");
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getPatronimic());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Shtolz");
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
         wd.findElement(By.name("nickname")).click();
         wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("BP");
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getAKA());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("Petrozavodsk, Lenina str. 15, 454");
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("(888)9991524");
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("asasas@trtr.net");
+        wd.findElement(By.name("email")).sendKeys(contactData.getMail());
         wd.findElement(By.name("bday")).click();
-        new Select(wd.findElement(By.name("bday"))).selectByVisibleText("15");
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getDay());
         wd.findElement(By.name("bday")).click();
         wd.findElement(By.name("bmonth")).click();
-        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("April");
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getMonth());
         wd.findElement(By.name("bmonth")).click();
         wd.findElement(By.name("byear")).click();
         wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys("1966");
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-        wd.findElement(By.linkText("Logout")).click();
-
+        wd.findElement(By.name("byear")).sendKeys(contactData.getYear());
     }
 
-    private void addNew() {
+    private void gotoAddNew() {
         wd.findElement(By.linkText("add new")).click();
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
