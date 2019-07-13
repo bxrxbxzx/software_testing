d116307f891630f7102389a3ba49a3ee96501db9
bxrxbxzx <ast_16@mail.ru>
2019-07-13 18:06:54 +0300

    Создан общий базовый класс

---
+++ addressbook-web-tests/src/test/java/ru/softwaretesting/addressbook/GroupDeletionTests.java
@@ -0,0 +1,19 @@
+package ru.softwaretesting.addressbook;
+
+
+import org.testng.annotations.Test;
+
+
+public class GroupDeletionTests extends TestBase {
+
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteGroup();
+        gotoGroupPage();
+    }
+
+}
+
