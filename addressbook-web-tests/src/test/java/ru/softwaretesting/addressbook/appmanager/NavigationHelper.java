e0adbf8818d2841c4e9826545964555e92bf693a
bxrxbxzx <ast_16@mail.ru>
2019-07-13 20:57:36 +0300

    Создали общий базовы класс для классов- помощников

--- addressbook-web-tests/src/test/java/ru/softwaretesting/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/softwaretesting/addressbook/appmanager/NavigationHelper.java
@@ -3,18 +3,14 @@ package ru.softwaretesting.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
-    public NavigationHelper() {
-
-    }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 }
